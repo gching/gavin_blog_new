079f6ab82ecb12b37b3ae1935344a982473e8c67
Gavin Ching <gavinchingy@gmail.com>
2013-10-14 01:11:39 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GavinBlogNew::Application.config.secret_key_base = '8f22b71116f51cb5e5ba2843b19ea6411bbc98b90d9a3dfd9e36e34c23b1e863f7ed6ab33faa10e003b646391abc86484609002cf4566e0a09af3d61ac1f9a45'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GavinBlogNew::Application.config.session_store :cookie_store, key: '_gavin_blog_new_session'
