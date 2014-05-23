e7e90a1a8f8a3d0cd234413a3010b386c4faa954
Gavin Ching <gavinchingy@gmail.com>
2014-05-22 17:11:09 -0700

    Change about me button and change favicon

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -3,15 +3,18 @@ require 'spec_helper'
 
 describe "When going through pages" do
 	let(:title) {"My Story A detailed book on my \"rife\" journey."}
-	it "have root path be the blog / story book" do
+	before :each do
 		visit '/'
+	end
+
+	it "have root path be the blog / story book" do
 		expect(page).to have_content title
 	end
 
 	it "should have a link to about page" do
-		click_link "About"
-		expect(page).to have content "About Me"
+		click_link "Who dat?"
+		expect(page).to have_content "Gavin Ching"
 	end
 
-	
+
 end
