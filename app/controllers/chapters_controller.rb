679ef44de89e7586120cd8d59a5fed680a1471a4
Gavin Ching <gavinchingy@gmail.com>
2014-07-10 23:33:11 -0700

    Remove redundant code in routes and controller

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -6,37 +6,6 @@ class ChaptersController < ApplicationController
 	def show
 	end
 
-	def new
-	end
-
-	## Removed as rails admin overides it.
-=begin
-	def create
-		@chapter = Chapter.new(permitted_params.chapter)
-		if @chapter.save
-			redirect_to chapter_path(@chapter)
-		else
-			render 'new'
-		end
-	end
-=end
-
-	def edit
-	end
-
-	def update
-		if chapter.update_attributes(permitted_params.chapter)
-			redirect_to chapter_path(chapter)
-		else
-			render 'edit'
-		end
-	end
-
-	def destroy
-		chapter.destroy
-		redirect_to root_path
-	end
-
 	## Provides feed for atom
 	## Not conventional
 	def feed
